3a9f68125eef283322b636d758e1cb1c9d49faf5
User <user@hackerrank.workspace>
2023-06-17 05:08:37 +0000

    Add initial repository
    Commit by HackerRank

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false);
+            // TODO Fill me in?
+        }
+    }
+}
